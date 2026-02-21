1739c0f0bc2994cbdf84be947e6db167cc4a717a
Harsh Agrawal <ha0744106@gmail.com>
2026-02-21 20:48:07 +0530

    Refactor kd_train_toy.sh for clarity and updates

--- scripts/kd_train_toy.sh
+++ scripts/kd_train_toy.sh
@@ -2,22 +2,20 @@
 # Copyright 2023. Nota Inc. All Rights Reserved.
 # Code modified from https://github.com/huggingface/diffusers/tree/v0.15.0/examples/text_to_image
 # ------------------------------------------------------------------------------------
-
 MODEL_NAME="CompVis/stable-diffusion-v1-4"
-TRAIN_DATA_DIR="./data/laion_aes/preprocessed_11k" # please adjust it if needed
+TRAIN_DATA_DIR="./data/laion_aes/preprocessed_11k"
 UNET_CONFIG_PATH="./src/unet_config"
-
-UNET_NAME="bk_small" # option: ["bk_base", "bk_small", "bk_tiny"]
-OUTPUT_DIR="./results/toy_"$UNET_NAME # please adjust it if needed
+UNET_NAME="bk_tiny" # option: ["bk_base", "bk_small", "bk_tiny"]
+OUTPUT_DIR="./results/toy_"$UNET_NAME
 
 BATCH_SIZE=2
 GRAD_ACCUMULATION=4
 
 StartTime=$(date +%s)
 
-CUDA_VISIBLE_DEVICES=1 accelerate launch src/kd_train_text_to_image.py \
+CUDA_VISIBLE_DEVICES=0 python src/kd_train_text_to_image.py \
   --pretrained_model_name_or_path $MODEL_NAME \
-  --train_data_dir $TRAIN_DATA_DIR\
+  --train_data_dir $TRAIN_DATA_DIR \
   --use_ema \
   --resolution 512 --center_crop --random_flip \
   --train_batch_size $BATCH_SIZE \
@@ -37,7 +35,5 @@ CUDA_VISIBLE_DEVICES=1 accelerate launch src/kd_train_text_to_image.py \
   --unet_config_path $UNET_CONFIG_PATH --unet_config_name $UNET_NAME \
   --output_dir $OUTPUT_DIR
 
-
 EndTime=$(date +%s)
 echo "** KD training takes $(($EndTime - $StartTime)) seconds."
-
